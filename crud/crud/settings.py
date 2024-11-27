c5a61db74fd4f0ed5098294511707383137d5254
Mahadi <smmahadi01910048684@gmail.com>
2024-11-27 16:12:07 +0600

    .remove csrf token

--- crud/crud/settings.py
+++ crud/crud/settings.py
@@ -45,7 +45,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'crud.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'crude',
+        'USER': 'mahadi',
+        'PASSWORD': '22322232',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
